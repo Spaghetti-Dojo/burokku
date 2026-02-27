7ea5f868d33c80195a00f1d2d717882056130a63
guido <dev@guidoscialfa.com>
2026-02-27 22:27:36 +0100

    Refactor style registration to use a version variable
    Simplify the version handling for enqueued styles by using a
    dedicated variable, improving readability and maintainability.

--- sources/server/Theme/Styles.php
+++ sources/server/Theme/Styles.php
@@ -23,23 +23,27 @@ final readonly class Styles
     private function register_styles(): void
     {
         $is_prod_env = wp_get_environment_type() === 'production';
+        $version = $is_prod_env
+            ? wp_get_theme()->get('Version')
+            : null;
+
         wp_enqueue_style(
             '@burokku/styles-atoms',
             get_theme_file_uri('dist/styles/atoms.css'),
             [],
-            $is_prod_env ? wp_get_theme()->get('Version') : null
+            $version
         );
         wp_enqueue_style(
             '@burokku/styles-molecules',
             get_theme_file_uri('dist/styles/molecules.css'),
             [],
-            $is_prod_env ? wp_get_theme()->get('Version') : null
+            $version
         );
         wp_enqueue_style(
             '@burokku/wp-class-utils',
             get_theme_file_uri('dist/styles/wp-class-utils.css'),
             [],
-            $is_prod_env ? wp_get_theme()->get('Version') : null
+            $version
         );
     }
 }
