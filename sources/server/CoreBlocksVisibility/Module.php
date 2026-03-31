010661eb744a006ad456cfdc60b9096111e65a7e
guido <dev@guidoscialfa.com>
2026-03-31 23:13:09 +0200

    feat(CoreBlocksVisibility): restrict allowed blocks for editor
    Introduce a new `CoreBlocksVisibility` module to control allowed blocks in the editor. Updated `package.php` to register the module.

---
+++ sources/server/CoreBlocksVisibility/Module.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpaghettiDojo\Burokku\CoreBlocksVisibility;
+
+use Inpsyde\Modularity\Module\ExecutableModule;
+use Inpsyde\Modularity\Module\ModuleClassNameIdTrait;
+use Psr\Container\ContainerInterface;
+
+class Module implements ExecutableModule
+{
+    use ModuleClassNameIdTrait;
+
+    private const array ALLOWED_BLOCKS = [
+        'core/paragraph',
+        'core/heading',
+        'core/buttons',
+        'core/button',
+        'core/list',
+        'core/list-item',
+        'core/quote',
+        'core/code',
+        'core/preformatted',
+        'core/table',
+        'core/image',
+        'core/accordion',
+        'core/accordion-heading',
+    ];
+
+    public static function new(): Module
+    {
+        return new self();
+    }
+
+    final private function __construct()
+    {
+    }
+
+    public function run(ContainerInterface $container): bool
+    {
+        add_filter('allowed_block_types_all', static fn() => self::ALLOWED_BLOCKS);
+
+        return true;
+    }
+}
